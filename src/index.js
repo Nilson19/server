bb198d7042069cb043d02d7ea26735b645f46b7e
Nilson28 <nilson.cardona@correounivalle.edu.co>
2020-04-07 16:07:25 -0500

    ruta a la raiz

--- src/index.js
+++ src/index.js
@@ -22,6 +22,11 @@ app.use('/api/escuelas', routerSchool);
 app.use('/api/materias', routerClass);
 app.use('/api/profesores', routerTeacher);
 app.use('/api/matriculas', routerRegistration);
+app.use('/', (req,res)=>{
+    res.json({
+        message: 'Bienvenido'
+    })
+});
 
 app.listen(3000, () =>{
     console.log('server on port 3000');
