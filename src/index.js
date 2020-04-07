7dea36b9285c539cadcd2697c8c607dfa4dcd8cd
Nilson28 <nilson.cardona@correounivalle.edu.co>
2020-04-07 15:57:32 -0500

    cambio de directorio

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+//importing routes
+const {routerStudent} = require('./routes/estudiantes');
+const {routerSchool} = require('./routes/escuelas');
+const {routerClass} = require('./routes/materias');
+const {routerTeacher} = require('./routes/profesores');
+const {routerRegistration} = require('./routes/matriculas');
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
+app.use('/api/estudiantes', routerStudent);
+app.use('/api/escuelas', routerSchool);
+app.use('/api/materias', routerClass);
+app.use('/api/profesores', routerTeacher);
+app.use('/api/matriculas', routerRegistration);
+
+app.listen(3000, () =>{
+    console.log('server on port 3000');
+});
